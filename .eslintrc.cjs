f075187befb7cb81fdb30ce7778e5153eb97518d
Ondra Svec <ondrasvecveslo@gmail.com>
2023-08-19 18:00:53 +0200

    Ensure eslint react-three-fiber compatibility

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,11 +7,12 @@ module.exports = {
     "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
     "prettier",
+    "plugin:@react-three/recommended",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
   settings: { react: { version: "18.2" } },
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "@react-three"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
