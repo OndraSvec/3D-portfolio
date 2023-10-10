63c577b4cac4afc1a724ccf3fc12677126f2a475
Ondra Svec <ondrasvecveslo@gmail.com>
2023-10-10 10:05:32 +0200

    Use react-three-drei Loader instead of a custom one

--- src/components/CanvasRenderer.jsx
+++ src/components/CanvasRenderer.jsx
@@ -1,22 +1,25 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
-import Loader from "./Loader";
 import { Suspense } from "react";
+import { Loader } from "@react-three/drei";
 
 const CanvasRenderer = () => (
-  <Canvas
-    flat
-    camera={{
-      fov: 45,
-      near: 0.1,
-      far: 2000,
-      position: [-4, 3, 5],
-    }}
-  >
-    <Suspense fallback={<Loader />}>
-      <Experience />
-    </Suspense>
-  </Canvas>
+  <>
+    <Canvas
+      flat
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 2000,
+        position: [-4, 3, 5],
+      }}
+    >
+      <Suspense>
+        <Experience />
+      </Suspense>
+    </Canvas>
+    <Loader dataStyles={{ fontSize: "1rem" }} />
+  </>
 );
 
 export default CanvasRenderer;
